7de475b5ac78cf57eea789a1a48d842f02e2b660
Scott <scottdlivingstone@gmail.com>
2017-11-01 10:53:46 -0700

    add scss to webpack process

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,37 @@ module.exports = {
   output: {
     filename: './build/bundle.js'
   },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      // ...other loaders...
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{
+          loader: 'file?name=public/fonts/[name].[ext]'
+        }]
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+    ]
+  }
 };
